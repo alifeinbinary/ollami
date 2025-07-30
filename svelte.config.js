54e6538658564a32b47f33ad1f94242c33fa11ac
Andrew Halliwell <andrew@alifeinbinary.com>
2025-07-30 03:52:56 -0700

    chore: project configuration. Switching to dynamic routes SSR.

--- svelte.config.js
+++ svelte.config.js
@@ -12,19 +12,10 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapterMulti([
-      adapterStatic({
-        pages: 'dist-front',
-        assets: 'dist-front',
-        fallback: undefined,
-        precompress: false,
-        strict: true,
-      }),
-      adapterNode({ 
-        out: 'dist-node', 
-        precompress: false 
-      })
-    ])
+    adapter: adapterNode({ 
+      out: 'dist-node', 
+      precompress: false 
+    })
   },
 };
 
