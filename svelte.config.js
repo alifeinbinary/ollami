798a7099f773387989444859e7f3d1f655d57e2b
Andrew Halliwell <andrew@alifeinbinary.com>
2025-07-30 01:03:48 -0700

    chore: adjusting and formatting front end configuration files for Pyloid.

--- svelte.config.js
+++ svelte.config.js
@@ -5,19 +5,27 @@ import adapterMulti from '@macfja/svelte-multi-adapter';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+  preprocess: vitePreprocess(),
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapterMulti([
-			adapterStatic({ pages: 'build-static', assets: 'build-static', precompress: false }),
-			adapterNode({ out: 'build-node', precompress: false }),
-		]),
-	}
+    adapter: adapterMulti([
+      adapterStatic({
+        pages: 'dist-front',
+        assets: 'dist-front',
+        fallback: undefined,
+        precompress: false,
+        strict: true,
+      }),
+      adapterNode({ 
+        out: 'dist-node', 
+        precompress: false 
+      })
+    ])
+  },
 };
 
 export default config;
